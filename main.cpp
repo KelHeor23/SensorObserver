98f5fb3e602b0e1b56290389d7beae81b27375d4
Alex <kelheor23@gmail.com>
2025-01-22 07:44:18 +0300

    MainWIn

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <QtWidgets/QApplication>
+
+#include "Widgets/MainWindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    MainWindow mainWindow;
+
+    mainWindow.show();
+
+    return a.exec();
+}

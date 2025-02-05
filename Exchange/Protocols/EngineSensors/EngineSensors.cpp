7c3a4c7cf6ee23a7e90416fd36c39ef3d1a1a129
Alex <kelheor23@gmail.com>
2025-02-05 09:10:03 +0300

    added engine sensor data parsing

--- Exchange/Protocols/EngineSensors/EngineSensors.cpp
+++ Exchange/Protocols/EngineSensors/EngineSensors.cpp
@@ -1,7 +1,22 @@
 #include "EngineSensors.h"
 
 EngineSensors::EngineSensors() {
-    sensorsData.push_back(NodeSensor{"Вибрация", 0, 0});
+    sensorsData.push_back(NodeSensor{"Обороты", 0, 0});
     sensorsData.push_back(NodeSensor{"Температура", 0, 0});
-    sensorsData.push_back(NodeSensor{"Что-то еще", 0, 0});
+    sensorsData.push_back(NodeSensor{"Угол биения", 0, 0});
+    sensorsData.push_back(NodeSensor{"Амплитуда биения", 0, 0});
+}
+
+void EngineSensors::EngineSensorsData::parseEngineSensorsData(const uint8_t *data)
+{
+    size_t offset = 0;
+    memcpy(&this->canID, data + offset, offset);
+    offset += sizeof(this->canID);
+    memcpy(&this->speed, data + offset, sizeof(this->speed));
+    offset += sizeof(this->speed);
+    memcpy(&this->temperature, data + offset, sizeof(this->temperature));
+    offset += sizeof(this->temperature);
+    memcpy(&this->runoutAngle, data + offset, sizeof(this->runoutAngle));
+    offset += sizeof(this->runoutAngle);
+    memcpy(&this->runoutAmplitude, data + offset, sizeof(this->runoutAmplitude));
 }
--- Exchange/Protocols/EngineSensors/EngineSensors.h
+++ Exchange/Protocols/EngineSensors/EngineSensors.h
@@ -7,10 +7,21 @@
 
 class EngineSensors: public Sensors
 {
+    struct EngineSensorsData {
+        uint32_t    canID;
+        uint16_t    speed;              // Обороты двигателя (0 - 65535), обор/м
+        int8_t      temperature;        // Температура двигателя (-128 ... +127), градусы/10
+        uint16_t    runoutAngle;        // Угол биения относительно метки на двигателе (0-359), градусы
+        uint16_t    runoutAmplitude;    // Амплитуда биения (0 - 65535), мили-g
+
+        void parseEngineSensorsData(const uint8_t* data);
+    };
+
 public:
     EngineSensors();
 
     std::vector<NodeSensor> sensorsData;
 };
+
 
 #endif // ENGINESENSORS_H

d061db1342af42a9ec443815418c937394530ee6
Alex <kelheor23@gmail.com>
2025-06-03 08:21:57 +0300

    add save/load settings

---
+++ Exchange/Protocols/SensorSettingsManager.h
@@ -0,0 +1,36 @@
+#ifndef SENSORSETTINGSMANAGER_H
+#define SENSORSETTINGSMANAGER_H
+
+#include <QSettings>
+#include <QHash>
+#include <QString>
+
+#include "Sensors.h"
+
+class SensorSettingsManager {
+public:
+    using SensorHashTable = std::unordered_map<QString, SensorData>;
+
+    static QString getConfigPath();
+
+    // Сохранить всю таблицу
+    static bool saveAll(const QString& organization, const QString& appName,
+                        const SensorHashTable& data);
+
+    // Загрузить всю таблицу
+    static bool loadAll(const QString& organization, const QString& appName,
+                        SensorHashTable& outData);
+
+    // Сохранить отдельный сенсор
+    static bool saveSensor(const QString& organization, const QString& appName,
+                           const QString& sensorName, const SensorData& data);
+
+    // Загрузить отдельный сенсор
+    static bool loadSensor(const QString& organization, const QString& appName,
+                           const QString& sensorName, SensorData& outData);
+
+    // Удалить отдельный сенсор
+    static bool removeSensor(const QString& organization, const QString& appName,
+                             const QString& sensorName);
+};
+#endif // SENSORSETTINGSMANAGER_H

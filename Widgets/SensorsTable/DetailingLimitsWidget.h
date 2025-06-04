51c91b49cba38f501ead8c59a644c7fd4a999fee
Alex <kelheor23@gmail.com>
2025-06-04 13:58:52 +0300

    add detailing limits widget

---
+++ Widgets/SensorsTable/DetailingLimitsWidget.h
@@ -0,0 +1,15 @@
+#ifndef DETAILINGLIMITSWIDGET_H
+#define DETAILINGLIMITSWIDGET_H
+
+#include <QtWidgets/QWidget>
+
+class DetailingLimitsWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit DetailingLimitsWidget(QWidget *parent = nullptr);
+
+signals:
+};
+
+#endif // DETAILINGLIMITSWIDGET_H

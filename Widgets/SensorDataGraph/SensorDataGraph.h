ff01045c435c82d535b8d94be64213c0a50d29cf
Alex <kelheor23@gmail.com>
2025-08-28 10:03:32 +0300

    add hash specificator for DataFullName

--- Widgets/SensorDataGraph/SensorDataGraph.h
+++ Widgets/SensorDataGraph/SensorDataGraph.h
@@ -8,21 +8,33 @@
 #include "Data/Frames/Containers/SensorsFrames.h"
 #include "qcustomplot.h"
 
+struct DataFullName {
+    size_t  engineNum;
+    QString frameName;
+    QString sensorName;
+
+    bool operator==(const DataFullName& other) const {
+        return engineNum == other.engineNum &&
+               frameName == other.frameName &&
+               sensorName == other.sensorName;
+    }
+};
+
+namespace std {
+template<>
+struct hash<DataFullName> {
+    size_t operator()(const DataFullName& key) const {
+        size_t h1 = qHash(key.frameName);
+        size_t h2 = qHash(key.sensorName);
+        size_t h3 = hash<size_t>()(key.engineNum);
+        return h1 ^ (h2 << 1) ^ (h3 << 2);
+    }
+};
+}
+
 class SensorDataGraph : public QWidget
 {
     Q_OBJECT
-
-    struct DataFullName {
-        size_t  engineNum;
-        QString frameName;
-        QString sensorName;
-
-        bool operator==(const DataFullName &other) const {
-            return engineNum == other.engineNum &&
-                   frameName == other.frameName &&
-                   sensorName == other.sensorName;
-        }
-    };
 
 public:
     explicit SensorDataGraph(std::shared_ptr<SensorsFrames> sensorsManager_t, QWidget *parent = nullptr);
